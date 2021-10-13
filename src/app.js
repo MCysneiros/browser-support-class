f2fa906379c57532857c8186daf657773eb01fdd
matheus cysneiros <msocys@gmail.com>
2021-10-13 19:50:33 -0300

    end

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
+// import 'core-js/features/promise';
+
 const button = document.querySelector('button');
 const textParagraph = document.querySelector('p');
 button.addEventListener('click', () => {
+  const promise = new Promise();
+  console.log(promise);
   const text = textParagraph.textContent;
-  navigator.clipboard
-    .writeText(text)
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((err) => console.log(err));
+  if (navigator.clipboard) {
+    navigator.clipboard
+      .writeText(text)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  } else {
+    alert('Feature not available ');
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,31 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
-    publicPath: 'assets/scripts/'
+    publicPath: 'assets/scripts/',
   },
   devtool: 'cheap-module-eval-source-map',
   // devServer: {
   //   contentBase: './'
   // }
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: 'defaults' },
+                { useBuiltIns: 'usage', corejs: { version: 3 } },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
